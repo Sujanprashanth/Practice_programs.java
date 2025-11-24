4a6fbdd014af4f6c39685d1a83de2e7081d1d89c
Sujan Prashanth P S <152985592+Sujanprashanth@users.noreply.github.com>
2025-11-25 01:08:36 +0530

    Update practice-programs.java

--- practice-programs.java
+++ practice-programs.java
@@ -2083,7 +2083,7 @@ class Main{
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the size of the array:");
 		int n = sc.nextInt();
-		int[] arr1 = new [n];
+		int[] arr1 = new int[n];
 		for(int i=0;i<n;i++){
 			arr1[i]=sc.nextInt();
 		}

8ae9193fcf7beea3180b73023fa5209c5afc974f
Sujan Prashanth P S <152985592+Sujanprashanth@users.noreply.github.com>
2025-12-06 20:05:07 +0530

    Fix syntax error in practice-programs.java

--- practice-programs.java
+++ practice-programs.java
@@ -2,7 +2,7 @@ Sum of digits in java:
 Program:
 class Main{
     public static void main(String[] args){
-        int n=1234,sum=0,A=0;
+        int n=1234,sum=0,A=0
         while(n!=0){
             A=n%10;
             sum+=A;

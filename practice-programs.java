2423cb4776d1fd9b63a9e37317dad66b61471f8d
Sujan Prashanth P S <152985592+Sujanprashanth@users.noreply.github.com>
2026-01-09 21:23:00 +0530

    Update practice-programs.java

--- practice-programs.java
+++ practice-programs.java
@@ -4394,3 +4394,41 @@ public class Main
 		System.out.print(b);
 	}
 }
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+7. Reverse Integer
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+Input: x = 123
+Output: 321
+
+Program:
+class Solution{
+    public int reverse(int x){
+        long rev=0;
+        int l=0;
+        while(x!=0){
+            l=x%10;
+            rev=(rev*10)+l;
+            x/=10;
+        }
+        if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE){
+            return 0;
+        }
+        else{
+            return (int) rev;
+        }
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int a = s.reverse(1268765456);
+        System.out.print(a);
+    }
+}
+
+Output:
+0
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------	

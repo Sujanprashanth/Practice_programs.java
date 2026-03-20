f6d11f87168d8adb3ac9453752e3bd78dafc79bc
Sujan Prashanth P S <152985592+Sujanprashanth@users.noreply.github.com>
2026-03-20 16:04:53 +0530

    Update practice-programs.java

--- practice-programs.java
+++ practice-programs.java
@@ -4683,3 +4683,72 @@ Helloworld
 3 7
 lowo
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+We define the following terms:
+Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Function Description
+Complete the getSmallestAndLargest function in the editor below.
+	
+getSmallestAndLargest has the following parameters:
+string s: a string
+int k: the length of the substrings to find
+Returns
+string: the string ' + "\n" + ' where and are the two substrings
+
+Input Format
+The first line contains a string denoting .
+The second line contains an integer denoting .
+
+Constraints
+consists of English alphabetic letters only (i.e., [a-zA-Z]).
+
+Sample Input
+welcometojava
+3
+	
+Sample Output 
+ava
+wel
+	
+Explanation 
+String  has the following lexicographically-ordered substrings of length :
+We then return the first (lexicographically smallest) substring and the last (lexicographically largest) substring as two newline-separated values (i.e., ava\nwel).
+The stub code in the editor then prints ava as our first line of output and wel as our second line of output.
+
+Program:
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String a="",b="";
+        int k=0;
+        Scanner sc = new Scanner(System.in);
+        a=sc.next();
+        sc.nextLine();
+        k=sc.nextInt();
+        sc.nextLine();
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<=a.length()-k;i++){
+            b=a.substring(i,i+k);
+            list.add(b);
+        }
+        Collections.sort(list);
+        System.out.println(list.get(0));
+        System.out.println(list.get(list.size()-1));
+    }
+}
+
+Output:
+Sample Input 
+welcometojava
+3
+	
+Sample Output 
+ava
+wel
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------

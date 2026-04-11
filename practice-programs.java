3152cb9aa8e5d7fa8730857786d750ffaff46572
Sujan Prashanth P S <152985592+Sujanprashanth@users.noreply.github.com>
2026-04-11 17:58:35 +0530

    Update practice-programs.java

--- practice-programs.java
+++ practice-programs.java
@@ -4866,3 +4866,89 @@ double → float → long → int → char → short → byte
 
 🔥 Super Simple Rule (Exam Hack)
 Direction	Type	
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+For this problem, we have  types of queries you can perform on a List:
+Insert  at index :
+Insert
+x y
+	
+Delete the element at index :
+Delete
+x
+	
+Given a list, , of  integers, perform  queries on the list. Once all queries are completed, print the modified list as a single line of space-separated integers.
+	
+Input Format
+The first line contains an integer,  (the initial number of elements in ).
+The second line contains  space-separated integers describing .
+The third line contains an integer,  (the number of queries).
+	
+The  subsequent lines describe the queries, and each query is described over two lines:
+If the first line of a query contains the String Insert, then the second line contains two space separated integers , and the value  must be inserted into  at index .
+If the first line of a query contains the String Delete, then the second line contains index , whose element must be deleted from .
+
+Constraints
+Each element in is a 32-bit integer.
+
+Output Format
+Print the updated list  as a single line of space-separated integers.
+
+Sample Input
+5
+12 0 1 78 12
+2
+Insert
+5 23
+Delete
+0
+	
+Sample Output
+0 1 78 12 23
+
+Program:
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        int size = sc.nextInt();
+        int b = 0,a1=0,a2=0,a3=0;
+        for(int i=0;i<size;i++){
+            b = sc.nextInt();
+            list.add(b);
+        }
+        int q = sc.nextInt();
+        String s ="";
+        for(int i=0;i<q;i++){
+            s=sc.next();
+            if(s.equalsIgnoreCase("Insert")){
+                a1=sc.nextInt();
+                a2=sc.nextInt();
+                list.add(a1,a2);
+            }
+            else{
+                a3 = sc.nextInt();
+                list.remove(a3);
+            }
+        }
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+" ");
+        }
+    }
+}
+
+Output:
+5
+12
+0
+1
+78
+12
+2
+Insert
+5
+23
+Delete
+0
+0 1 78 12 23
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------

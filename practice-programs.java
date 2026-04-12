924226ee6fb2204ea1758fc64d0f82cd36224b55
Sujan Prashanth P S <152985592+Sujanprashanth@users.noreply.github.com>
2026-04-12 13:53:24 +0530

    Update practice-programs.java

--- practice-programs.java
+++ practice-programs.java
@@ -4952,3 +4952,77 @@ Delete
 0
 0 1 78 12 23
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+You are given a phone book that consists of people's names and their phone number. After that you will be given some person's name as query. For each query, print the phone number of that person.
+
+Input Format
+The first line will have an integer  denoting the number of entries in the phone book. Each entry consists of two lines: a name and the corresponding phone number.
+After these, there will be some queries. Each query will contain a person's name. Read the queries until end-of-file.
+
+Constraints:
+A person's name consists of only lower-case English letters and it may be in the format 'first-name last-name' or in the format 'first-name'. Each phone number has exactly 8 digits without any leading zeros.
+
+Output Format
+For each case, print "Not found" if the person has no entry in the phone book. Otherwise, print the person's name and phone number. See sample output for the exact format.
+To make the problem easier, we provided a portion of the code in the editor. You can either complete that code or write completely on your own.
+
+Sample Input
+3
+uncle sam
+99912222
+tom
+11122222
+harry
+12299933
+uncle sam
+uncle tom
+harry
+	
+Sample Output
+uncle sam=99912222
+Not found
+harry=12299933
+
+Program:
+import java.util.*;
+class Main{
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		Map<String,Integer> lmap = new HashMap<>();
+		String name="";
+		int number=0;
+		for(int i=0;i<size;i++){
+			name=sc.nextLine();
+			number=Integer.ParseInt(sc.nextLine());
+			lmap.put(name,number);
+		}
+		while(sc.hasNextLine()){
+			String query = sc.nextLine();
+			if(hmap.containsKey(query)){
+				System.out.println(query+"="+lmap.get(query));
+			}
+			else{
+				System.out.println("Not found");
+			}
+		}
+	}
+}
+
+Output:
+Sample Input
+3
+uncle sam
+99912222
+tom
+11122222
+harry
+12299933
+uncle sam
+uncle tom
+harry
+	
+Sample Output
+uncle sam=99912222
+Not found
+harry=12299933
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------

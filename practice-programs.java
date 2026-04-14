3ff9b7d85347c8640c7225e010c7ec8c2445250a
Sujan Prashanth P S <152985592+Sujanprashanth@users.noreply.github.com>
2026-04-14 21:19:19 +0530

    Add Move Zeroes algorithm implementation
    Implemented the 'Move Zeroes' algorithm to shift all zeros to the end of the array while maintaining the order of non-zero elements.

--- practice-programs.java
+++ practice-programs.java
@@ -5071,4 +5071,42 @@ Input
 nums = [2,7,11,15]
 target = 9
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+283. Move Zeroes
+Hint
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
 	
+Example 2:
+Input: nums = [0]
+Output: [0]
+
+Constraints:
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1	
+
+Program:
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        while(j < nums.length){
+            nums[j] = 0;
+            j++;
+        }
+        System.out.print(Arrays.toString(nums));
+    }
+}
+
+Output:
+Input
+nums = [0,1,0,3,12]
+Stdout = [1, 3, 12, 0, 0]
